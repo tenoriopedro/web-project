3af7ffbefe41d91508f9e4316706009fded4914a
tenoriopedro <pstsouza13@gmail.com>
2025-05-02 21:20:20 +0100

    Adjusting the banner.

--- djangoweb/website/static/website/js/banners.js
+++ djangoweb/website/static/website/js/banners.js
@@ -1,12 +1,12 @@
-document.addEventListener(
-    'DOMContentLoaded', function () {
-        const glide = new Glide('.glide', {
-            type: 'carousel',
-            autoplay: 4000,
-            hoverpause: true,
-            animationDuration: 800,
-            perView: 1
-        });
+// document.addEventListener(
+//     'DOMContentLoaded', function () {
+//         const glide = new Glide('.glide', {
+//             type: 'carousel',
+//             autoplay: 4000,
+//             hoverpause: true,
+//             animationDuration: 800,
+//             perView: 1
+//         });
         
-        glide.mount();
-});
+//         glide.mount();
+// });
