9a1c9816ab8e2e03673cf983a082a5e14aa81a33
tenoriopedro <pstsouza13@gmail.com>
2025-08-17 03:41:18 +0100

    Creating the HTML and CSS for the page that will list the products chosen by the user.

--- djangoweb/products/views.py
+++ djangoweb/products/views.py
@@ -19,15 +19,33 @@ class ProductsListView(ListView):
     model = Products
     template_name = 'products/list_products.html'
     context_object_name = 'products'
+    paginate_by = 9
+
 
     def get_queryset(self):
         category_slug = self.kwargs['slug_category']
-        category = ProductsSetup.objects.get(
-            slug_category=category_slug
-        )
-        return Products.objects.filter(
+        category = ProductsSetup.objects.get(slug_category=category_slug)
+
+        queryset = Products.objects.filter(
             product_type=category.product_type
         )
+
+        order = self.request.GET.get('order', 'name')
+        if order in ['name', '-name', 'id', '-id']:
+            queryset = queryset.order_by(order)
+
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        slug_category = self.kwargs.get('slug_category')
+
+        context['current_order'] = self.request.GET.get('order', 'name')
+        context['slug_category'] = slug_category
+
+        return context
+        
 
 
 class ProductDetailView(DetailView):
