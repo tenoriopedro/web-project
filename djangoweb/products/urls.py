5951ed845e7e85d3aeca0a70f0d573fe7067655b
tenoriopedro <pstsouza13@gmail.com>
2025-08-23 00:04:21 +0100

    Creating a new view ProductsSearchView

--- djangoweb/products/urls.py
+++ djangoweb/products/urls.py
@@ -1,19 +1,32 @@
 from django.urls import path
-from products.views import ProductsIndexView, ProductsListView, ProductDetailView
+from products.views import ProductsIndexView, ProductsListView, ProductDetailView, ProductsSearchView
 
 app_name = 'products'
 
 urlpatterns = [
+
+    # Index Page
     path('', ProductsIndexView.as_view(), name='index'),
+
+    # Search Page
+    path(
+        "pesquisa/",
+        ProductsSearchView.as_view(),
+        name="products-search"
+    ),
+
+    # List Page
     path(
         '<slug:slug_category>/', 
         ProductsListView.as_view(), 
         name='product-list'
     ),
     
+    # Detail Page
     path(
         '<slug:slug_category>/<slug:slug_product>/',
         ProductDetailView.as_view(),
         name='product-detail'
     ),
+
 ]
--- djangoweb/products/views.py
+++ djangoweb/products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Products, ProductsSetup
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 
 class ProductsIndexView(TemplateView):
@@ -27,7 +28,7 @@ class ProductsListView(ListView):
         category = ProductsSetup.objects.get(slug_category=category_slug)
 
         queryset = Products.objects.filter(
-            product_type=category.product_type
+            product_type=category
         )
 
         order = self.request.GET.get('order', 'name')
@@ -40,13 +41,17 @@ class ProductsListView(ListView):
 
         context = super().get_context_data(**kwargs)
         slug_category = self.kwargs.get('slug_category')
+        name_category = ProductsSetup.objects.get(
+            slug_category=slug_category
+        )
 
         context['current_order'] = self.request.GET.get('order', 'name')
         context['slug_category'] = slug_category
+        context['name_category'] = name_category
+        context["is_search"] = False
 
         return context
         
-
 
 class ProductDetailView(DetailView):
     model = Products
@@ -54,10 +59,8 @@ class ProductDetailView(DetailView):
     context_object_name = 'products'
     slug_url_kwarg = "slug_product"
 
-
     def get_object(self):
 
-        slug_category = self.kwargs.get("slug_category")
         slug_product = self.kwargs.get("slug_product")
 
         return get_object_or_404(
@@ -80,3 +83,31 @@ class ProductDetailView(DetailView):
         
         return context
     
+
+class ProductsSearchView(ListView):
+    model = Products
+    template_name = 'products/list_products.html'
+    context_object_name = "products"
+    paginate_by = 8
+
+    def get_queryset(self):
+        query = self.request.GET.get("q", "")
+        queryset = Products.objects.all().select_related("product_type")
+
+        if query:
+
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(short_description__icontains=query) |
+                Q(product_type__slug_category__icontains=query)
+            )
+
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["q"] = self.request.GET.get("q", "")
+        context["is_search"] = True
+
+        return context
