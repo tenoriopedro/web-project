0d485e818f1e97001aca8a937c24b5c0a0d86fb3
tenoriopedro <pstsouza13@gmail.com>
2025-08-22 23:34:37 +0100

    Adjusting the function according to the new Products model

--- djangoweb/utils/image_upload_path.py
+++ djangoweb/utils/image_upload_path.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def product_image_upload_path(instance, filename):
 
     base_folder = 'image_products_setup' if instance._meta.model_name == 'productssetup' else 'image_products'
-    product_type = slugify(instance.product_type.text if instance.product_type else "sem-tipo")
+    product_type = slugify(instance.product_type.slug_category if instance.product_type else "sem-tipo")
 
     return os.path.join(
         base_folder,
