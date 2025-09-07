da33144193d3c0bfe422d1a04d027bf878484025
tenoriopedro <pstsouza13@gmail.com>
2025-09-07 23:06:41 +0100

    Creating a generic function to serve other functions in different scenarios

--- djangoweb/utils/save_check.py
+++ djangoweb/utils/save_check.py
@@ -1,16 +1,28 @@
-from utils.images import resize_image
+from utils.images import resize_image, resize_image_product
 
-def check_favicon(favicon, current_favicon_name, favicon_changed):
-    favicon_changed_def = favicon_changed
 
-    if favicon:
-        favicon_changed_def = current_favicon_name != favicon.name
+def _check_image(image, current_image_name, size):
 
-    if favicon_changed_def:
-        new_image = resize_image(favicon, 64)
+    image_changed = False
+
+    if image:
+        image_changed = current_image_name != image.name
+
+    if image_changed:
+
+        resize_func = resize_image_product if size >= 600 else resize_image
+        new_image = resize_func(image, size)
 
         return new_image
 
-    else:
-        return
+    return
+
+def check_favicon(image, current_image_name):
+    return _check_image(
+        image, current_image_name, size=64
+    )
     
+def check_product_image(image, current_image_name):
+    return _check_image(
+        image, current_image_name, size=800
+    )
