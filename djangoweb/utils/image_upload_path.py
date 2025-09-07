e68b87829c22445459c6e6b1811ff923f14e4428
tenoriopedro <pstsouza13@gmail.com>
2025-09-07 23:15:08 +0100

    Condition to search for the correct value of the models

--- djangoweb/utils/image_upload_path.py
+++ djangoweb/utils/image_upload_path.py
@@ -6,7 +6,16 @@ from datetime import datetime
 def product_image_upload_path(instance, filename):
 
     base_folder = 'image_products_setup' if instance._meta.model_name == 'productssetup' else 'image_products'
-    product_type = slugify(instance.product_type.slug_category if instance.product_type else "sem-tipo")
+    if instance._meta.model_name == 'productssetup':
+        product_type = slugify(
+            instance.product_type.text 
+            if instance.product_type else "sem-tipo"
+        )
+    else:  # Products
+        product_type = slugify(
+            instance.product_type.slug_category 
+            if instance.product_type else "sem-categoria"
+        )
 
     return os.path.join(
         base_folder,
